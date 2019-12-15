d1a9a1fd95c756fbdffb48350fb40a1bbeb0ee8c
Arina Dragon <dragonarina@gmail.com>
2019-12-15 21:49:48 +0300

    completed

---
+++ 3Sem/OOP/Labs/Lab14_15/src/Hodor.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Hodor {
+    private static Map<String, String> map = new HashMap<String, String>();
+
+       public static  Map<String, String> Stark() {
+           map.put("Winona", "Ryder");
+           map.put("Keanu", "Reeves");
+           map.put("Sandra", "Bullock");
+           map.put("Joaquin", "Phoenix");
+           map.put("Leonardo", "DiCaprio");
+           map.put("Johnny", "Depp");
+           map.put("Margot", "Robbie");
+           map.put("Bradley", "Cooper");
+           map.put("Lady", "Gaga");
+           map.put("Jennifer", "Lawrence");
+           return map;
+       }
+
+
+    public static int getName(HashMap<String, String> map, String name)
+    {
+        int count = 0;
+        for (Map.Entry<String, String> pair : map.entrySet())
+        {
+            String value = pair.getValue();
+            if (value.equals(name))
+                count++;
+        }
+        return count;
+    }
+
+    public static int getSurname(HashMap<String, String> map, String surname)
+    {
+        int count = 0;
+        for (Map.Entry<String, String> pair : map.entrySet())
+        {
+            if (pair.getKey().equals(surname))
+                count++;
+        }
+        return count;
+    }
+}
