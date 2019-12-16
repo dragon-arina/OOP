af4ca96ba290de9b8f825c434eaf61ea2285feb5
Arina Dragon <dragonarina@gmail.com>
2019-12-16 21:59:06 +0300

    completed

--- 3Sem/OOP/Labs/Lab14_15/src/Test.java
+++ 3Sem/OOP/Labs/Lab14_15/src/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         HashMap<String, String> hm = (HashMap<String, String>) Hodor.Stark();
-        System.out.println(Hodor.getSameFirstNameCount(hm, "Depp"));
-        System.out.println(Hodor.getSameLastNameCount(hm, "Johnny"));
+        System.out.println(Hodor.getSameFirstNameCount(hm, "Johnny"));
+        System.out.println(Hodor.getSameLastNameCount(hm, "Depp"));
 
     }
 }
