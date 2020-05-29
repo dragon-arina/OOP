f5b65468da7e3389e0f1efe1295e33c572339278
Arina Dragon <dragonarina@gmail.com>
2020-05-29 17:50:00 +0300

    commit

---
+++ 4Sem/SDPA/2-13/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_75d3a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_75d3a.dir/testCXXCompiler.cxx.o"
+  "cmTC_75d3a"
+  "cmTC_75d3a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_75d3a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 4Sem/SDPA/2-13/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_75d3a.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_75d3a.
+# This may be replaced when dependencies are built.
