d43e5a89e146c581bf3b5b288d93c143f935c6b9
Arina Dragon <dragonarina@gmail.com>
2020-05-31 19:54:14 +0300

    Android seventh lab

--- 4Sem/laba7/app/src/androidTest/java/com/example/laba7/ExampleInstrumentedTest.java
+++ 4Sem/laba7/app/src/androidTest/java/com/example/laba7/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.laba_7", appContext.getPackageName());
+        assertEquals("com.example.laba7", appContext.getPackageName());
     }
 }
